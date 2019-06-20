877217bc217179a53f14cd850e18abbe5d4aff3d
Margot Konig <margot.konig@blackrock.com>
2019-06-20 01:40:47 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,4 +1,4 @@
- class AddFavoriteFoodToArtists < ActiveRecord::Migration
+ class AddColumnsToArtists < ActiveRecord::Migration[5.1]
   def change
     add_column :artists, :favorite_food, :string
   end
