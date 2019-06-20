885742ed63a059c4e63a338fda69e476fb84aa9c
Margot Konig <margot.konig@blackrock.com>
2019-06-20 01:44:25 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,5 +1,5 @@
- class ChangeColumnsToStudents < ActiveRecord::Migration[5.1]
+ class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
   def change
-    change_column :students, :grade, :datetime 
+    change_column :students, :birthdate, :datetime 
   end
 end
